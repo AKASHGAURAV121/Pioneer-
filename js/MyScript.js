8dbc1de28ba89ba04dbb365a7bd7d517ee5274a8
Akash <akki.grv20@gmail.com>
2022-06-12 14:42:04 +0530

    first commit

---
+++ js/MyScript.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+
+   // alert("Hello");
+
+   $("#formSubmit").on("click",function(){
+
+    var number=$("#number").val();
+    var pan=$("#pan").val();
+    var pincode=$("#pincode").val();
+    var aincome=$("#aincome").val();
+    console.log(number+pincode);
+
+
+});
+
+});
+
+
